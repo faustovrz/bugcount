135cd95288911c1dbca5cf6fb3b0389d8975e24b
faustovrz <faustovrz@gmail.com>
2017-09-19 19:39:02 -0500

    Initial commit

---
+++ analysis.R
@@ -0,0 +1,335 @@
+# Whitefly analysis.
+
+#setwd("C:/Users/faustorodriguez/Desktop")
+setwd("~/Desktop")
+source('bugcountGLM.R')
+
+# Start pdf output device for all subsequent plots
+
+# pdf.file <- 'resistance_analysis.pdf'
+# pdf(pdf.file, onefile=TRUE)
+
+# 1. Read WF count data. #######################################################
+#
+# WF counts per picture. Each picture must have a unique combination of
+# experiment + replicate + plant + pic
+# experiment description consists of:
+# exp.year + exp.cross + exp.propagation + exp.substrate
+# **************************************************************************** #
+
+wf <- read.wf(file="WF_consolidated.tab", sep="\t",header=TRUE)
+
+# 2. Counts per Plant/leaf. ######################################
+
+wf.leaf <- leaf.count(wf)
+summary(wf.leaf$nymphs)
+
+wf.plant <- plant.count(wf)
+summary(wf.plant$nymphs)
+
+# Pick analysis level plant or leaf
+# wf.count <- wf.plant
+
+wf.count <- wf.leaf
+
+# **************************************************************************** #
+#
+#  What indicator should we use as resistance phenotype?
+#
+#  Nymph counts are not normal 
+#  Error is not distributed normally.
+#  Nymph counts do not meet linear model assumptions (ANOVA, MapQTL).
+#  Best fit is a negative Binomial Distribution
+#
+# **************************************************************************** #
+
+# Probability density model fit to actual distribution
+
+wf.count.fit <-  count.fit(wf.count)
+
+quartz()
+plot.count.fit(wf.count.fit, main = "Nymphs")
+
+
+# 3. Reproducibility ##########################################################
+
+# Assume that nymphs per plant are distributed as negative binomial
+
+# **************************************************************************** #
+# 3.1 Whole population.
+
+
+# nymphs ~ experiment GLM, posthoc and common letter difference 
+by.experiment <- fit.nb.glm(nymphs ~ experiment,wf.count)
+
+quartz()
+plot.fit.nb.glm(wf.count, by.experiment)
+
+
+quartz()
+plot.fit.nb.glm(wf.count,by.experiment,type="density")
+
+# add infestation regime as factor 
+#    high,low: inferred from nyphs ~ experiment GLM
+
+wf.count$infestation <- "low"
+wf.count[wf.count$exp.year == 2017,]$infestation <- "high"
+wf.count$infestation <- factor(wf.count$infestation, levels =c("low","high"))
+
+# I need a single normally distributed number per cassava accesion
+# for QTL analysis
+
+# log transfromed geometric mean 
+# Robust against extreme values 
+
+# **************************************************************************** #
+# Correlation  between experiments 
+
+cor.title <- paste("Geometric Means Correlation","Log Scale", sep = "\n")
+
+for (cross in levels(wf.count$exp.cross)) {
+  
+  wf.by.exp <- wf.wide(clone~experiment,
+                       wf.count[wf.count$exp.cross==cross,])
+  
+  quartz()
+  
+  plot.wide.cor(wf.by.exp, main = paste(cross,cor.title))
+  
+}
+
+
+
+# **************************************************************************** #
+# 3.2 Checks.
+
+wf.check <- wf.count[grepl("check", wf.count$group) & wf.count$clone != "Secundina",]
+wf.check <- remove.levels(wf.check)
+names(wf.check)
+levels(wf.check$clone)
+
+# Correlation  between experiments
+
+check.by.exp <- wf.wide(clone ~ experiment, wf.check, 
+                        fun = function(x) log10(geometric.mean(x)) )
+levels(wf.check$clone)
+
+quartz()
+plot.check.cor(check.by.exp, main = paste("Checks", cor.title))
+
+head(wf.check)
+# nymphs ~ clone GLM posthoc and common letter difference
+fit <- fit.nb.glm(nymphs ~ clone, wf.check)
+
+quartz()
+plot.fit.nb.glm(wf.check,fit)
+
+quartz()
+plot.fit.nb.glm(wf.check,fit,type = "density")
+
+
+# nymphs ~ infestation * clone GLM
+
+fit <- glm.nb(nymphs ~  infestation * clone, data = wf.check)
+# lsmeans(fit, ~ infestation * clone,
+#         adjust = "tuckey", transform = "response") 
+# lsmeans with transorm = "response" might give geometric means
+# and geomeric means ratio tests and confidence intervals I need to confirm!!!
+
+posthoc <- cld(lsmeans(fit, ~ infestation * clone, adjust = "tuckey"),
+               type = "response")
+
+
+# Plot post hoc
+
+### Order the levels for printing
+clone.order <- wf.aggregate(wf.check, by.stat = "gmean")$clone
+
+posthoc$clone <- factor(posthoc$clone,
+                      levels=clone.order)
+
+posthoc$infestation = factor(posthoc$infestation,
+                   levels=c("low", "high"))
+
+###  Remove spaces in .group  
+
+posthoc$.group=gsub(" ", "", posthoc$.group)
+
+quartz()
+plot.gg.incfestationXclone(posthoc)
+
+
+quartz()
+plot.infestationXclone(wf.check)
+
+
+# Nymph counts by Cross #########################################################
+# Select just the clones where there are more than 2 experiments
+cross <-"CM8996"
+exp.levels <-levels(as.factor(wf.count$experiment))
+exp.grp <-list(1,2,4,5,c(1,2),c(2,4),c(4,5),c(1,2,4), c(2,4,5))
+for (idx in 1:length(exp.grp)){
+exp.allowed <- exp.levels[exp.grp[[idx]]]
+
+wf.by.cross <- wf.count[wf.count$exp.cross==cross &
+                          wf.count$experiment %in% exp.allowed,]
+
+exp.count <- aggregate(experiment ~ clone ,wf.by.cross,
+                       FUN = function(x) length(unique(x)))
+selected.clones <- exp.count$clone[exp.count$experiment==length(exp.allowed)]
+wf.by.cross <- wf.by.cross[ wf.by.cross$clone %in%selected.clones,]
+wf.by.cross <- remove.levels(wf.by.cross)
+
+wf.by.cross$nymphs<-wf.by.cross$nymphs+1
+
+# quartz()
+# hist(log10(wf.by.cross$nymphs))
+
+by.cross.count.fit <-  count.fit(wf.by.cross[wf.by.cross$group == "offspring",])
+
+# quartz()
+# plot.count.fit(by.cross.count.fit, 
+#                main = paste(c(exp.allowed,"Nymphs"), collapse ="\n"))
+
+
+# ANOVA ********************************************************************* #
+# Assuming Normal distribution
+#
+tail(wf.by.cross)
+fit.lm <- lm(nymphs ~  clone, data = wf.by.cross)
+AIC(fit.lm)
+posthoc <- cld(lsmeans(fit.lm, "clone", adjust = "tuckey"))
+colnames(posthoc)
+
+### Order the levels for printing
+# clone.order <- wf.aggregate(wf.by.cross, by.stat = "gmean")$clone
+clone.order <- posthoc$clone
+posthoc$clone <- factor(posthoc$clone,
+                      levels=clone.order)
+###  Remove spaces in .group  
+
+posthoc$.group=gsub(" ", "", posthoc$.group)
+
+lm.post <- posthoc
+
+# quartz()
+# print(ggplot(lm.post,
+#          aes(x     = clone,
+#              y     = lsmean,
+#              color = .group,
+#              label = .group),
+#          log10="y") +
+#     geom_point(shape  = 15,
+#                size   = 4) +
+#     geom_errorbar(aes(ymin  =  lower.CL, 
+#                       ymax  =  upper.CL),
+#                   width =  0.2,
+#                   size  =  0.7) +
+#     theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) +
+#     #      legend.position =  c(0.9, 0.2) ) +
+#     # scale_y_continuous(trans=log10_trans(), limits = c(1,10000) ) +
+#     ggtitle(paste(c(exp.allowed,"ANOVA, Mean + 95% Confidence Interval"),
+#                   collapse ="\n")))
+
+
+# GLM ********************************************************************** #
+# Assuming Negative Binomial distribution
+#
+
+fit.nb <- glm.nb(nymphs ~  clone, data = wf.by.cross)
+AIC(fit.nb)
+anova(fit.lm,fit.nb)
+
+posthoc <- cld(lsmeans(fit.nb, ~ clone, adjust = "tuckey"),
+               type = "response")
+colnames(posthoc)
+
+# Plot post hoc
+# plot(posthoc)
+
+### Order the levels for printing
+# clone.order <- wf.aggregate(wf.by.cross, by.stat = "gmean")$clone
+clone.order <- posthoc$clone
+posthoc$clone <- factor(posthoc$clone,
+                      levels=clone.order)
+
+###  Remove spaces in .group  
+
+posthoc$.group=gsub(" ", "", posthoc$.group)
+
+nb.glm.post <- posthoc
+
+quartz()
+print(ggplot(nb.glm.post,
+         aes(x     = clone,
+             y     = response,
+             color = .group,
+             label = .group),
+         log10="y") +
+    geom_point(shape  = 15,
+               size   = 4) +
+    geom_errorbar(aes(ymin  =  asymp.LCL, 
+                      ymax  =  asymp.UCL),
+                  width =  0.2,
+                  size  =  0.7) +
+    theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5),
+          legend.position="none") +
+    scale_y_continuous(trans=log10_trans(),limits = c(0.001,100000)) +
+    ggtitle(paste(c(exp.allowed,
+                  "Negative Binomial GLM, Mean + 95% Confidence Interval"),
+                  collapse ="\n")))
+}
+
+p <- merge(lm.post, nb.glm.post, by="clone")
+
+quartz()
+plot(response~lsmean, data=p)
+
+
+
+# 4. Ressistance Efficacy #####################################################
+
+# Calculate ressitance efficacy per experiment
+# and Correlation between experiments
+
+control <- "COL1468"
+
+ef<- efficacy.aggregate(wf.count, control = control)
+
+# Clone Sample Size by Experiment
+
+quartz()
+par(mar=c(12,4,4,2)) 
+boxplot(clone_n ~ experiment, data = ef, 
+        main = "Clone Sample Size by Experiment",
+        las = 2, log ="y")
+
+quartz()
+plot.ef.density(ef,"geometric.mean.eff","experiment")
+
+quartz()
+par(mar=c(15,4,0,2)) 
+boxplot(asin(geometric.mean.eff) ~ experiment, las = 2,
+        ylab = "asin(Efficacy)", data=ef)
+
+# Correlation  between experiments
+
+cor.title <- paste("Efficacy Correlation","asin Transform", sep = "\n")
+
+for (cross in levels(ef$exp.cross)) {
+
+# remove outlying point for correlations
+ef.by.cross <- ef[ef$exp.cross == cross & ef$clone != control,] 
+ef.by.cross <-remove.levels(ef.by.cross)
+
+ef.gmean <-efficacy.list(ef.by.cross)$geometric.mean
+
+quartz()
+plot.ef.cor(ef.gmean,main = paste(cross,"/",control,cor.title))
+
+}
+
+# graphics.off()
+
+# 5. Sample size calculations #################################################
+
---
+++ bugcountGLM.R
@@ -0,0 +1,484 @@
+library(car)
+library(MASS)
+library(ggjoy)
+library(scales) 
+library(reshape2)
+library(psych)
+library(lsmeans)
+library(multcomp)
+
+# WF counts per picture. Each picture must have a unique combination of
+# experiment + replicate + plant + pic
+# experiment description consists of:
+# exp.year + exp.cross + exp.propagation + exp.substrate
+# **************************************************************************** #
+if(.Platform$OS.type=="windows") {
+  quartz<-function() windows()
+}
+
+exp.descriptor <- function(wf){
+  if (any(!grepl("experiment",colnames(wf)))){
+    descriptor <- grep("exp.",colnames(wf), value=TRUE)
+    c("experiment",exp.descriptor)
+    } else {
+      descriptor <- grep("exp.",colnames(wf), value=TRUE)
+    }
+  descriptor
+}
+
+exp.criteria <- function(wf){
+  paste(exp.descriptor(wf), collapse= " + ")
+  
+} 
+
+plant.descriptor <-  function(wf){
+  c("group","propagation","substrate","clone", "rep","plant")
+}
+
+plant.criteria <- function(wf){
+  paste(c(exp.descriptor(wf),plant.descriptor(wf)), collapse= " + ")
+}
+
+
+leaf.descriptor <- function(wf){
+  c(plant.descriptor(wf),"leaf")
+}
+
+leaf.criteria <-function(wf){
+  paste(c(exp.descriptor(wf),leaf.descriptor(wf)), collapse= " + ")
+}
+
+
+read.wf <- function(file,...){
+  wf<- read.table(file="WF_consolidated.tab", sep="\t",header=TRUE)
+  wf<-wf[!(is.na(wf$nymphs)),]
+  # Add experiment as factor = exp.propagation + exp.substrate + exp.year
+  wf$experiment <- apply( wf[, exp.descriptor(wf) ] ,
+                          1 , paste , collapse = "_" )
+  wf
+  # TODO: order columns with espriment at the beginning. new function?
+}
+
+leaf.count <- function(wf){
+ aggregate(as.formula(paste ("nymphs ~ ",  leaf.criteria(wf)) ), 
+                       data = wf, FUN=sum, na.rm=TRUE)
+} 
+
+plant.count <- function(wf){
+  
+  wf.nleaf <- aggregate(as.formula(paste ("leaf ~ ",  plant.criteria(wf))),
+                        data=leaf.count(wf), FUN=length)
+  
+  wf.plant <- aggregate(as.formula(paste ("nymphs ~ ",plant.criteria(wf))),
+                        data=wf.leaf, FUN=sum)
+  wf.plant$nleaf <- wf.nleaf$leaf
+  wf.plant[wf.plant$nleaf>1,]
+}
+
+
+
+#' Geometric mean of a vector ignoring NAs.
+#' 
+#' @param x A vector.
+#' @return Geometric mean of  \code{x} ignoring NAs.
+#'
+#' @examples
+#' gm.mean(1:1000)
+
+geometric.mean = function(x, na.rm=TRUE){
+  exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
+}
+
+
+#' Equivalent to `MASS::fitdistr(x, densfun = "gamma")`, where x are first rescaled to 
+#' the appropriate scale for a gamma distribution. Useful for fitting the gamma distribution to 
+#' data which, when multiplied by a constant, follows this distribution
+#' @param x A vector.
+#' @return MASS::fitdistr with scaling multiplier
+#'
+#' @examples
+#' fitgamma(1:1000)
+
+fitgamma <- function(x) {
+  if (!requireNamespace("MASS")) stop("Requires MASS package.")
+  
+  fit <- glm(formula = x ~ 1, family = Gamma)
+  out <- MASS::fitdistr(x * coef(fit), "gamma")
+  out$scaling_multiplier <- unname(coef(fit))
+  out
+}
+
+
+remove.levels <-function(df){
+  df[] <- lapply(df, function(x) if(is.factor(x)) factor(x) else x)
+  df
+} 
+
+#' wf.aggregate
+#' @param wf A whitefly count dataframe.
+#' @param x criterion column for aggregation.
+#' @param y variable column for aggregation.
+#' @param by.stat column for aggregation.
+#' @param ascending sort order.
+#' @return Dataframe of aggregates by specified {col} 
+#'         counts ordered by \code{by.stat}
+#'
+#' @examples
+#' fitgamma(1:1000)
+
+wf.aggregate <- function (wf,x="clone", y="nymphs", by.stat ="mean", ascending= TRUE){
+  form <- formula(paste(y,x, sep = " ~ "))
+  wf.ag <- aggregate(form, data=wf,
+                      FUN=function(x) c(n = length(x),
+                                        mean  = mean(x, na.rm=TRUE), 
+                                        gmean = geometric.mean(x),
+                                        median = median(x,na.rm=TRUE)))
+  # fix aggregate column names
+  wf.ag <- fix.ag.colnames(wf.ag)
+
+  # sort by.stat
+  wf.ag <-  wf.ag[order(wf.ag[,paste(y,by.stat,sep = ".")]),]
+                        
+  # sort x levels by.stat
+  # wf.sort <- function(wf,by.stat){}
+  wf.ag[,x] <- factor(wf.ag[,x], levels=wf.ag[,x])
+  wf.ag
+}
+
+
+count.fit <- function(wf){
+  poisson <- fitdistr(wf$nymphs, "Poisson")
+  nbinom <- fitdistr(wf$nymphs, "Negative Binomial")
+  #gammad <- fitgamma(wf.plant$nymphs+1)
+  
+  nymph.hist <-  hist(wf$nymphs, breaks=50, plot=FALSE)
+  mids <- nymph.hist$mids
+  
+  list( hist =nymph.hist,
+        nb.d = dnbinom(mids,
+                       mu = nbinom$estimate[[2]],
+                       size = nbinom$estimate[[1]]),
+        
+        lnorm.d = dlnorm(mids,
+                         meanlog = mean(log(wf$nymphs+1)),
+                         sdlog = sd(log((wf$nymphs+1)))),
+        
+        norm.d = dnorm(mids,
+                        mean = mean(wf$nymphs),
+                        sd = sd(wf$nymphs)),
+        
+        pois.d = dpois(mids,
+                        lambda =poisson$estimate[[1]])
+  )
+}
+
+
+plot.count.fit <- function(x,...){
+  plot(x$hist, xlab = "Nypmh Count",...)
+  lines(x$hist$mids,max(x$hist$counts) * x$nb.d / max(x$nb.d),
+         col = gg_color_hue(4)[1], lwd=2, lty = 1)
+  lines(x$hist$mids,max(x$hist$counts) * x$lnorm.d / max(x$lnorm.d),
+        col = gg_color_hue(4)[2], lwd=2, lty = 2)
+  lines(x$hist$mids,max(x$hist$counts) * x$pois.d / max(x$pois.d),
+        col = gg_color_hue(4)[3], lwd=2, lty = 3)
+  lines(x$hist$mids,max(x$hist$counts) * x$norm.d / max(x$norm.d),
+        col = gg_color_hue(4)[4], lwd=2, lty = 4)
+  
+  # logspline aproximation, not good.
+  # lines(wf.hist$mids,max(wf.hist$counts)*logspline.d/max(logspline.d))
+  
+  legend("topright",legend=factor(c("Negative Binomial", "Lognormal","Poisson","Normal"),
+                                  levels = c("Negative Binomial","Lognormal", "Poisson","Normal")),
+         title = "Model Aproximation",
+         col= gg_color_hue(4), lwd = 2, lty=1:4, bty ="n")
+  
+  # Q-Q plots
+  
+  # Innecesary for now
+  # lnorm means lognormal
+  
+
+  # qqp(wf.plant$nymphs, ylab = "Nymph Counts")
+  
+
+  # qqp(wf.plant$nymphs + 1, "lnorm", ylab = "Nymph Counts")
+
+  # qqp(wf.plant$nymphs, "pois", poisson$estimate, ylab = "Nymph Counts")
+  
+  
+
+  # qqp(wf.plant$nymphs, "nbinom", 
+  #     size = nbinom$estimate[[1]], 
+  #     mu = nbinom$estimate[[2]],
+  #     ylab = "Nymph Counts")
+  
+
+  # qqp(wf.plant$nymphs+1, "gamma", 
+  #     shape = gammad$estimate[[1]],
+  #     rate = gammad$estimate[[2]])
+  recordPlot()
+}
+
+
+fit.nb.glm <-function(formula,wf){
+  # assign("data",wf, .GlobalEnv) # Nasty!!! Ruins the scope!
+  # TODO: validate formula for one predictor only
+  response <- as.character(terms(formula)[[2]])
+  predictor <- as.character(terms(formula)[[3]])
+  fit <- glm.nb(formula, data = wf)
+  lsm.fit <- lsmeans(fit, predictor,
+                     adjust = "tuckey",
+                     data = wf)
+  ls.posthoc <- cld(lsm.fit, type = "response")
+  #ls.posthoc$experiment <- gsub(" ","",ls.posthoc$.group)
+  list( formula = formula,
+        response = response,
+        predictor = predictor,
+        glm = fit, lsm = lsm.fit,
+        posthoc = ls.posthoc,
+        order = ls.posthoc[,predictor],
+        group = as.factor(ls.posthoc$.group)
+        )
+}
+
+
+plot.fit.nb.glm <- function(wf, nb.fitted, type = "CI", xmax =15000){
+  group <- nb.fitted$group
+  n.grp <- length(levels(group))
+  grp.col <- gg_color_hue(n.grp)[group]
+  response <- nb.fitted$response
+  predictor <- nb.fitted$predictor
+  order <- nb.fitted$order
+  
+    if(type=="CI"){
+    plot(nb.fitted$posthoc , col = grp.col, 
+         lwd =25, xlab = "Nymphs", 
+         ylab = as.character(group),
+         main = "Arithmetic Mean Confidence Interval \n from GLM")
+      
+    } else if(type=="density"){
+    
+    # change factor for joyplot to appear in the same order as posthoc plot
+    wf[,predictor] <- factor(wf[,predictor], levels = order)
+    ggplot(wf, aes_string(x = response, y = predictor, fill = predictor))  + xlim(0, xmax) +
+       ggtitle("Density") + geom_joy() + 
+       scale_fill_manual(values = grp.col) +
+       theme(legend.position="none")
+    
+  }# TODO: else{stop("")}
+}
+
+plot.infestationXclone <-function(wf){
+  # TODO:  validate that infestation is a column of wf
+  wf.aggregate(wf, by.stat = "gmean")
+  clone.order <- wf.aggregate(wf, by.stat = "gmean")$clone
+  n.clones <-length(clone.order)
+  
+  wf$clone <- factor(wf$clone, levels = clone.order)
+  
+  par(mar=c(6,4,4,2)) 
+  boxplot(nymphs+1 ~  infestation + clone, data=wf, frame = FALSE,
+          border = "white", log="y", xaxt="n", yaxt="n")
+  
+  rect(seq(0.5,2*n.clones, by=4),
+       rep(1,n.clones),
+       seq(2.5,2*n.clones+0.5, by=4),
+       rep(100000,n.clones),
+       col="grey90",lty=0)
+  
+  boxplot(nymphs+1 ~  infestation + clone, las=2, log = "y",
+          main = "Interaction: Infestation Regime x Clone",
+          ylab = "Nymphs per Unit",xaxt="n", xlab = "",
+          col= c("white", "grey"),
+          data = wf, add=TRUE)
+  
+  axis(1, at=2*(1:n.clones)-0.5,labels=clone.order, las=2)
+  
+  mtext("Clone (sorted by geometric mean of nymphs)", side=1, line=5)
+  
+  legend(2*n.clones - 3,6, legend=c("High", "Low"),
+         title = "Infestation",
+         bg = "white",
+         fill=c("grey", "white"))
+}
+
+plot.gg.incfestationXclone<-function(cld){
+  title <- "Interaction: Infestation Regime x Clone"
+  subtitle <- "(GLM Arithmetic Means + 95% Confidence Interval)"
+
+  ggplot(cld,
+         aes(x     = clone,
+             y     = response,
+             color = infestation,
+             label = .group),
+         log10="y") +
+    geom_point(shape  = 15,
+               size   = 4) +
+    geom_text(aes( x= clone,
+                   y = asymp.UCL,
+                   label=.group),
+                   nudge_y = 0.1) +
+    geom_errorbar(aes(ymin  =  asymp.LCL,
+                      ymax  =  asymp.UCL),
+                  width =  0.2,
+                  size  =  0.7) +
+    theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5), 
+          legend.position =  c(0.9, 0.2) ) +
+    scale_y_continuous(trans=log10_trans(),
+                       limits = c(1,10000),
+                       breaks =c(1,10,100,1000,10000)) +
+    ggtitle(paste(title,subtitle, sep = "\n"))
+}
+
+
+
+wf.wide <- function(formula, wf, fun = geometric.mean){
+  dcast(wf,formula,
+        value.var = "nymphs",
+        fun.aggregate = fun)
+}
+
+# plot.wide.cor<-function(wf.wide, fun = "Geometric Means"){
+#   pairs.panels(log10(wf.wide[,-1]), smooth = FALSE, density=FALSE,
+#                ellipses=FALSE, rug = FALSE, pch = 1, lm= TRUE, stars = TRUE,
+#                main = paste(cross,fun, "Correlation"))
+# }
+
+
+efficacy.aggregate<- function (wf, control = "COL1468"){
+  wf <-wf.count
+  form <- formula(nymphs ~ exp.cross + experiment + clone + group)
+  wf.ag <- aggregate(form, data=wf,
+                     FUN=function(x) c(n = length(x),
+                                       mean  = mean(x, na.rm=TRUE), 
+                                       geometric.mean = geometric.mean(x),
+                                       median = median(x,na.rm=TRUE)))
+  # fix aggregate column names
+  wf.ag <- fix.ag.colnames(wf.ag)
+
+  wf.x <- wf.ag[wf.ag$group == "infestation_check" & wf.ag$clone == control,
+                !(colnames(wf.ag) %in% c("clone", "group","exp.cross"))]
+  colnames(wf.x) <- gsub("nymphs.","infestation_",
+                         colnames(wf.x),fixed=TRUE)
+  colnames(wf.x) <- gsub("nymphs.","infestation_",
+                         colnames(wf.x),fixed=TRUE)
+  wf.y <- wf.ag[wf.ag$group != "infestation_check",
+                colnames(wf.ag) != "group"]
+  colnames(wf.y) <- gsub("nymphs.","clone_",
+                         colnames(wf.y),fixed=TRUE) 
+  wf.merge <- merge(wf.x, wf.y,
+              by="experiment",
+              all.y = TRUE)
+
+  wf.y <- wf.ag[wf.ag$group != "infestation_check" & wf.ag$clone == control,
+                !(colnames(wf.ag) %in% c("clone", "group","exp.cross"))]
+  colnames(wf.y) <- gsub("nymphs.","control_",
+                         colnames(wf.y),fixed=TRUE)
+  
+  wf.merge <- merge(wf.merge, wf.y,
+                    by="experiment",
+                    all.y = TRUE)
+  
+  wf.merge$mean.eff <- 1 - wf.merge$clone_mean/wf.merge$control_mean
+  wf.merge$geometric.mean.eff <- 1 - wf.merge$clone_geometric.mean/wf.merge$control_geometric.mean
+  wf.merge$median <- 1 - wf.merge$clone_median/wf.merge$control_median
+  wf.merge
+}
+
+efficacy.list <- function(ef){
+list(mean = dcast(ef,clone ~ experiment,value.var = "mean.eff"),
+     geometric.mean = dcast(ef,clone ~ experiment,value.var = "geometric.mean.eff"))
+#   median = dcast(wf.merge,clone ~ experiment,value.var = "median"))
+
+}
+
+
+plot.ef.density <-function(ef,type, group){
+  n.group <- length(unique(ef[,group]))
+  ggplot(ef, 
+         aes_string(x = type,
+                    y = group,
+                    fill = group)) +
+    ggtitle("Efficacy Distribution") + 
+    geom_joy() +  
+    xlim(-2, max(ef[,type])) +
+    scale_fill_cyclical(values = gg_color_hue(n.group)) +
+    xlab("Efficacy")  
+}
+
+# fix aggregate column names
+fix.ag.colnames <- function (ag){
+  do.call(cbind.data.frame,ag)
+}
+
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
+
+panel.hist <- function(x, ...)
+{ usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(usr[1:2], 0, 1.5) )
+  h <- hist(x, plot = FALSE)
+  breaks <- h$breaks; nB <- length(breaks)
+  y <- h$counts; y <- y/max(y)
+  rect(breaks[-nB], 0, breaks[-1], y, col="cyan", ...)
+}
+
+panel.points <- function(x,y){
+  points(x,y)
+  abline(stats::lm(y ~ x), col = "red")
+}
+
+panel.cor <- function(x, y, digits=2, prefix="")
+{
+  usr <- par("usr"); on.exit(par(usr))
+  par(usr = c(0, 1, 0, 1))
+  r <- abs(cor(x, y, use="pairwise.complete.obs"))
+  txt <- format(c(r, 0.123456789), digits=digits)[1]
+  txt <- paste(prefix, txt, sep="")
+  cex <- 0.4/strwidth(txt)
+  
+  test <- cor.test(x,y)
+  # borrowed from printCoefmat
+  Signif <- symnum(test$p.value, corr = FALSE, na = FALSE,
+                   cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                   symbols = c("***", "**", "*", ".", " "))
+  
+  text(0.5, 0.5, txt, cex = cex, col = "darkgrey")
+  text(.65, .6, Signif, cex = cex , col = "darkgrey", pos= 4)
+}
+
+plot.wide.cor<-function(wf.wide, main = "Correlation", ...){
+  pairs(log10(wf.wide[,-1]),
+        lower.panel = panel.points,
+        diag.panel = panel.hist,
+        upper.panel = panel.cor, 
+        main = main)
+}
+
+plot.check.cor <-function(check.wide,
+                          main = "Correlation",
+                          ...){
+  pairs(check.wide[,-1],
+        lower.panel = function(x,y){
+          text(x,y, labels=check.wide$clone, cex= 1)
+          abline(stats::lm(y ~ x), col = "red")
+        },
+        labels = gsub("_","\n", colnames(check.wide[,-1])),
+        upper.panel = panel.cor, 
+        gap = 0,
+        main = main
+        )
+}
+
+
+plot.ef.cor<-function(ef.par, main = "Correlation", ...){
+  pairs(asin(ef.par[,-1]),
+        lower.panel = points.panel,
+        diag.panel = panel.hist,
+        upper.panel = panel.cor,
+        main = main)
+}
+
+  
