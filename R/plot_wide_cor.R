f3fbb7e9ed5cecae78fdfaa1035e2a87e072be2d
faustovrz <faustovrz@gmail.com>
2018-05-04 17:17:49 -0500

    updated documentation: plot_wide_cor

--- R/plot_wide_cor.R
+++ R/plot_wide_cor.R
@@ -5,7 +5,7 @@
 #'
 #' @examples
 #' # plot_wide.cor(efficacy.list(ef)$mean)
-
+#' @export
 plot_wide_cor <- function(wf_wide, main = "Correlation", ...) {
   pairs(log10(wf_wide[,-1]),
         lower.panel = panel_points,
